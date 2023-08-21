c1dfb23b79e33bb09228a0f6acab184da196d00f
rca <raphael.catolino@gmail.com>
2023-08-21 13:27:10 +0200

    finish msgbuf rpc util

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::collections::VecDeque;
+use std::io::{copy, ErrorKind, Read, Result};
 use std::net::IpAddr;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,4 +16,55 @@ pub struct PeerDef {
     pub peer_key: Vec<u8>,
     pub endpoint: IpAddr,
     pub allowed_ips: Vec<(IpAddr, u8)>,
+}
+
+pub struct MsgBuf {
+    inner: VecDeque<u8>,
+    position: usize,
+}
+
+impl Read for MsgBuf {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let (first_slice, second_slice) = self.inner.as_slices();
+        let src = if self.position < first_slice.len() {
+            &first_slice[self.position..]
+        } else if self.position - first_slice.len() < second_slice.len() {
+            &second_slice[self.position - first_slice.len()..]
+        } else {
+            return Ok(0);
+        };
+
+        let copy_size = std::cmp::min(buf.len(), src.len());
+        buf[0..copy_size].copy_from_slice(&src[0..copy_size]);
+        self.position += copy_size;
+        Ok(copy_size.try_into().unwrap())
+    }
+}
+
+impl MsgBuf {
+    pub fn new() -> Self {
+        MsgBuf {
+            inner: VecDeque::with_capacity(16),
+            position: 0,
+        }
+    }
+
+    pub fn drain<T: Read>(&mut self, source: &mut T) -> Result<usize> {
+        let previous_size = self.inner.len();
+        match copy(source, &mut self.inner) {
+            Err(e) if e.kind() == ErrorKind::WouldBlock => Ok(self.inner.len() - previous_size),
+            Ok(_) => Ok(self.inner.len() - previous_size),
+            Err(e) => Err(e),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+
+    pub fn consume(&mut self, amt: usize) {
+        self.position = 0;
+        self.inner.drain(..amt);
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,11 @@
-use crate::rpc::{Message, PeerDef};
+use crate::rpc::{Message, MsgBuf, PeerDef};
 use crate::wireguard::list;
 use clap::ArgMatches;
-use mio::{Events, Poll, Interest, Token, };
 use mio::net::TcpListener;
+use mio::{Events, Interest, Poll, Token};
 use nix::ifaddrs::getifaddrs;
 use serde_json::Deserializer;
+use std::collections::HashMap;
 use std::io::Error as IoError;
 use std::io::ErrorKind;
 use std::net::{IpAddr, SocketAddr};
@@ -40,12 +41,13 @@ pub fn server_main(args: &ArgMatches) -> std::io::Result<()> {
     for (index, mut addr) in getsockaddrs(&wgifname).enumerate() {
         let filter = args.get_one::<IpAddr>("address");
         if filter.is_some() && Some(&addr.ip()) != filter {
-            continue
+            continue;
         }
 
         addr.set_port(*args.get_one::<u16>("port").expect("default"));
         listeners.push(TcpListener::bind(addr)?);
-        poll.registry().register(&mut listeners[index], Token(index), Interest::READABLE)?;
+        poll.registry()
+            .register(&mut listeners[index], Token(index), Interest::READABLE)?;
         println!(
             "Using wireguard interface {} and address {:?}",
             wgifname, addr
@@ -64,17 +66,56 @@ pub fn server_main(args: &ArgMatches) -> std::io::Result<()> {
     });
 
     let mut events = Events::with_capacity(128);
+    let mut clients = HashMap::new();
+    let mut count = listeners.len() + 1;
     loop {
         poll.poll(&mut events, None)?;
         for event in events.iter() {
-            let (s, _) = listeners[event.token().0].accept()?;
-            serde_json::to_writer(&s, &msg)?;
-            let msg_stream = Deserializer::from_reader(&s).into_iter::<Message>();
-            for msg in msg_stream {
-                println!("New message : {:?}", msg);
-                match msg? {
-                    Message::GetPeerList => serde_json::to_writer(&s, &Server::get_peer_list())?,
-                    _ => println!("Unsupported message"),
+            // println!("New event : {:?}", event);
+            let token = event.token().0;
+            if token < listeners.len() {
+                // Listener event
+                let (mut s, _) = listeners[token].accept()?;
+                poll.registry()
+                    .register(&mut s, Token(count), Interest::READABLE)?;
+                clients.insert(count, (s, MsgBuf::new()));
+                count += 1;
+            } else if token > listeners.len() {
+                // Client event
+                // serde_json::to_writer(&s, &msg)?;
+                let (stream, buffer) = clients
+                    .get_mut(&token)
+                    .expect("Polled event from non existing client !");
+
+                let drained = buffer.drain(stream)?;
+                let mut should_close = false;
+                if drained == 0 {
+                    should_close = true;
+                } else {
+                    println!("Read {} bytes from client {}.", drained, token);
+                    let consumed = {
+                        let mut msgstream =
+                            Deserializer::from_reader(&mut *buffer).into_iter::<Message>();
+                        for msg in msgstream.by_ref() {
+                            match msg {
+                                Ok(Message::GetPeerList) => {
+                                    println!("Received message GetPeerList");
+                                    serde_json::to_writer(&mut *stream, &Server::get_peer_list())?
+                                }
+                                Err(e) => println!("Error deserializing msg : {:?}", e),
+                                Ok(m) => println!("Unsupported message {:?}", m),
+                            }
+                        }
+
+                        msgstream.byte_offset()
+                    };
+
+                    buffer.consume(consumed);
+                }
+
+                if event.is_read_closed() || should_close {
+                    poll.registry().deregister(stream)?;
+                    clients.remove(&token);
                 }
             }
         }
